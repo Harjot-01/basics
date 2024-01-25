d44699c0010dda197a8615fe4b248c4f9a4cbf5f
Harjot-01 <157690626+Harjot-01@users.noreply.github.com>
2024-01-25 15:13:17 +0530

    commit changes

---
+++ JS/script.js
@@ -0,0 +1,18 @@
+// Calculator functions
+function appendToDisplay(value) {
+    document.getElementById('display').value += value;
+}
+
+function clearDisplay() {
+    document.getElementById('display').value = '';
+}
+
+function calculateResult() {
+    var displayValue = document.getElementById('display').value;
+    try {
+        var result = eval(displayValue);
+        document.getElementById('display').value = result;
+    } catch (error) {
+        document.getElementById('display').value = 'Error';
+    }
+}
